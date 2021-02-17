181a338634df990a044bc4a76d22e5c05d619e19
Alex Messersmith <alex.knifesmith@gmail.com>
2021-02-17 17:06:16 -0600

    Clean up and comment out AccessoryPosition.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/AccessoryPosition.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/AccessoryPosition.java
@@ -1,5 +1,16 @@
 package org.firstinspires.ftc.teamcode.util;
 
+/**
+ * The {@link AccessoryPosition} of a certain subsystem
+ * notates its current position, or is used to move it to a new position
+ * This is often used for servos or motors to run to certain positions
+ */
 public enum AccessoryPosition {
-    OPEN, CLOSED, UP, DOWN, MIDDLE, ENGAGED, DISENGAGED
+    OPEN,
+    CLOSED,
+    UP,
+    DOWN,
+    MIDDLE,
+    ENGAGED,
+    DISENGAGED
 }
