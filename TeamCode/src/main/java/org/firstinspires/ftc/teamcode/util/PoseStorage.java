df0ccbc3a9a214db664c3f8c67f39f908f409d68
Alex Messersmith <alex.knifesmith@gmail.com>
2021-03-25 17:22:29 -0500

    Adjust posing for new shooter

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/PoseStorage.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/PoseStorage.java
@@ -8,7 +8,7 @@ public class PoseStorage {
     public static Pose2d POSE = new Pose2d(-63.0, 40.0, Math.toRadians(0.0));
     public static final Pose2d AUTO_POSE = new Pose2d(-63.0, 40.0, Math.toRadians(180.0));
 
-    public static final Pose2d SHOOTING_POSE = new Pose2d(-2.0, 32.0, Math.toRadians(180.0));
+    public static final Pose2d SHOOTING_POSE = new Pose2d(-2.0, 24.0, Math.toRadians(180.0));
 
     public static final Pose2d POWERSHOT_POSE = new Pose2d(0.0, -15.0, Math.toRadians(180.0));
 }
